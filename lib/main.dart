45a4cacfa98ed0d2f1b3d92409910c6120fdfecc
Winson G <96694861+winsongr@users.noreply.github.com>
2022-07-24 21:25:07 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:olx/screens/login_screen.dart';
-import 'package:olx/screens/splash_screen.dart';
 
 void main() async {
   await Firebase.initializeApp();
